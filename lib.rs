e35e56a1f62dafb7476026d6bb12600b15c031be
shin.takahashi <realtakahashi.0924@gmail.com>
2023-05-01 09:19:34 +0900

    example was finished to implement

--- lib.rs
+++ lib.rs
@@ -4,9 +4,13 @@
 mod account_id_test {
     use ink::prelude::{ vec, vec::Vec };
     use ink::prelude::string::{ String, ToString };
+    use ink::prelude::format;
     use scale::{Decode, Encode};
+    use core::str::FromStr;
+    // use sp_core::crypto::Ss58Codec;
+    // use sp_core::H256;
 //    use sp_runtime::AccountId32;
-    
+   
     /// Defines the storage of your contract.
     /// Add new fields to the below struct in order
     /// to add new static storage fields to your contract.
@@ -15,6 +19,17 @@ mod account_id_test {
         /// Stores a single `bool` value on the storage.
         value: bool,
     }
+
+    // use ink_env::AccountId; 
+    // use sp_core::H256; 
+    // use core::convert::From;  
+    // impl From<AccountId> for H256 {
+    //     fn from(account_id: AccountId) -> Self {
+    //          let mut h256 = H256::default();
+    //          h256.as_mut().copy_from_slice(&account_id[..]);         
+    //          h256     
+    //     } 
+    // }
 
     impl AccountIdTest {
         /// Constructor that initializes the `bool` value to the given `init_value`.
@@ -31,11 +46,46 @@ mod account_id_test {
         }
 
         #[ink(message)]
-        pub fn convert_accountid_to_string2(&self, account_id: AccountId) -> String {
-            let tmp :Vec<u8> = account_id.encode();
-            tmp
-            //String::from_utf8(tmp).unwrap()
+        pub fn convert_accountid_to_hexstring(&self, account_id: AccountId) -> String {
+            hex::encode(account_id)
+            // let account_id: &[u8;32] = account_id.as_ref();
+            // let account_id = H256::from(account_id);
+
+            // // Convert the AccountId to an SS58 encoded string
+            // let account_id_string = account_id.to_ss58check();
+            // account_id_string
         }
+
+        #[ink(message)]
+        pub fn convert_hexstring_to_accountid(&self, hex_string:String) -> Option<AccountId> {
+            match hex::decode(hex_string) {
+                Ok(value) => {
+                    let mut array = [0; 32];
+                    let bytes = &value[..array.len()];
+                    array.copy_from_slice(bytes);
+                    let account_id: AccountId = array.into();
+                    return Some(account_id);
+                },
+                Err(_) => None,
+            }
+        }
+
+        // #[ink(message)]
+        // pub fn convert_accountid_to_string2(&self, account_id: AccountId) -> String {
+        //     // let mut output = vec![0; 32];
+        //     //let mut output:Vec<u8> = Vec::new();
+        //     let mut output = [0];
+        //     // let mut output: String = "Hello, world!".to_string();
+        //     // let mut output2 = output.into_bytes();
+        //     match bs58::encode(account_id).into(&mut output){
+        //         Ok(_) => (),
+        //         Err(_) => return "".to_string(),
+        //     };
+        //     match String::from_utf8(output) {
+        //         Ok(value) => value,
+        //         Err(_) => "".to_string(),
+        //     }
+        // }
 
         #[ink(message)]
         pub fn convert_string_to_accountid(&self, account_str: String) -> Option<AccountId> {
